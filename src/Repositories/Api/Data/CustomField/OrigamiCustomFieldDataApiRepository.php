7f36c9f8995506d2c92a5517fc08754597f80414
Valerie <valerie@comptoirducode.com>
2026-08-06 18:38:45 +0200

    Handle CustomField and CustomFieldValue

--- src/Repositories/Api/Data/CustomField/OrigamiCustomFieldDataApiRepository.php
+++ src/Repositories/Api/Data/CustomField/OrigamiCustomFieldDataApiRepository.php
@@ -2,7 +2,7 @@
 
 namespace OrigamiMp\OrigamiApiSdk\Repositories\Api\Data\CustomField;
 
-use OrigamiMp\OrigamiApiSdk\Dtos\CustomField\CustomFieldListDto;
+use OrigamiMp\OrigamiApiSdk\Dtos\CustomField\CustomFieldDefinitionListDto;
 use OrigamiMp\OrigamiApiSdk\Dtos\CustomField\CustomFieldValueListDto;
 use OrigamiMp\OrigamiApiSdk\ParamBags\Data\CustomField\ListCustomFieldRequestParamBag;
 use OrigamiMp\OrigamiApiSdk\ParamBags\Data\CustomField\ListCustomFieldValueRequestParamBag;
@@ -10,12 +10,12 @@ use OrigamiMp\OrigamiApiSdk\Repositories\Api\Data\OrigamiDataApiRepository;
 
 class OrigamiCustomFieldDataApiRepository extends OrigamiDataApiRepository
 {
-    public function list(ListCustomFieldRequestParamBag $paramBag): CustomFieldListDto
+    public function list(ListCustomFieldRequestParamBag $paramBag): CustomFieldDefinitionListDto
     {
         $response = $this->restClient->get('custom_fields', $paramBag);
         $responseContent = json_decode($response->getBody()->getContents());
 
-        return new CustomFieldListDto($responseContent);
+        return new CustomFieldDefinitionListDto($responseContent);
     }
 
     /**
