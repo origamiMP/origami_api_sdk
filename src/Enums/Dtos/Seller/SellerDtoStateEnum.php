6aa800c6e7bb4ef3179028c8c3e0de71dc13125d
Valerie-Tylski-Vincent <80036331+Valerie-Tylski-Vincent@users.noreply.github.com>
2026-09-25 14:05:26 +0200

    Complete enum for SellerState (#29)

--- src/Enums/Dtos/Seller/SellerDtoStateEnum.php
+++ src/Enums/Dtos/Seller/SellerDtoStateEnum.php
@@ -4,5 +4,7 @@ namespace OrigamiMp\OrigamiApiSdk\Enums\Dtos\Seller;
 
 enum SellerDtoStateEnum: string
 {
+    case REGISTERED = 'REGISTERED';
+    case WAITING = 'WAITING';
     case VALIDATED = 'VALIDATED';
 }
